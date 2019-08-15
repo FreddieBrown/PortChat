b1155abe9eb9e4cea9e38deaa19ad466e9e2fc3a
Freddie Brown <fred@noser.net>
2019-08-15 23:41:44 +0100

    Started writing function which listens to STDIN

---
+++ src/threads.c
@@ -0,0 +1,50 @@
+#include "threads.h"
+
+
+// void* sendMessage(void * arg) {
+//     //Take port and monitor message queue
+//     int flag = 1;
+//     int valread;
+//     do{
+//         // Once message is found, send via port
+//         send(new_socket, message, strlen(message), 0);
+//         char data;
+//         recv(new_socket,&data,1, MSG_PEEK);
+//     }while(flag);
+// }
+
+// void* receiveMessage(void * arg) {
+//     // Get port and setup message queue
+//     int flag = 1;
+//     int valread;
+//     do{
+//         // Read messages sent via port
+//         valread = read(new_socket, buffer, 1024);
+//         char * exit = "exit\n";
+//         // Print message
+//         printf("%s\n", buffer);
+//         char * message = "\nAck\n\n";
+//         send(new_socket, message, strlen(message), 0);
+//         // test port connection
+//         char data;
+//         flag = strcmp(buffer, exit);
+//         if(!flag){
+//             close(new_socket);
+//         }
+//         recv(new_socket,&data,1, MSG_PEEK);
+//         memset(buffer, 0, sizeof(buffer));
+//     }while(flag);
+// }
+
+void* createMessage(void* arg){
+    // Read from command line, create message 
+    // and pass it to message queue
+    int flag = 1;
+    char* buffer = malloc(256 * sizeof(char));
+    while(flag) {
+        scanf("%s\n", buffer);
+        // Add to message queue
+        printf("Output: %s\n", buffer);
+        memset(buffer, 0, sizeof(buffer));
+    }
+}
