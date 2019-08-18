dbccafd4082caaadd0eaad744a1f1b1071384a92
Freddie Brown <fred@noser.net>
2019-08-18 01:16:34 +0100

    Added comments and got 2 way chat working

--- src/threads.c
+++ src/threads.c
@@ -1,46 +1,61 @@
 #include "threads.h"
 
-
-void* sendMessage(void * arg) {
-    struct thread * info = (thread*) arg;
-    struct queue * que = info->que;
-    //Take port and monitor message queue
-    int flag = 1;
-    do{
-        // Once message is found, send via port
-        if(info->flag){
-            return arg;
-        }
-
-        if(que->len > 0) {
-            struct data * dat = pull(que);
-            printf("Message is: %s\n", dat->message);
-            printf("Socket: %i\n", info->socket);
-            if (send(info->socket, dat->message, strlen(dat->message), 0) < 0) {
-                puts("Send failed");
-            }
-            free(dat);
-        }
-    }while(flag);
-    return arg;
-}
-
+/**
+ * @brief Creates and sends a message
+ * 
+ * Takes in a thread struct and uses it to send 
+ * messages via an open port from the commandline.
+ * 
+ * @param arg Informaiton for the thread
+ * @return void* struct which was passed into the function
+ */
 void* createMessage(void* arg){
     thread * info = (thread*) arg;
+    char buffer[1024] = {0};
     // Read from command line, create message 
     // and pass it to message queue
-    int flag = 1;
-    char buffer[1024] = {0};
-    while(flag) {
-        if(info->flag){
+    while(1) {
+        fgets (buffer, 100, stdin);
+        send(info->socket, buffer, strlen(buffer), 0);
+        if(!*(info->flag)){
+            printf("Goodbye\n");
             return arg;
         }
         memset(buffer, 0, sizeof(buffer));
-        scanf("%s[\n]", buffer);
-        struct data * dat = malloc(sizeof(struct data));
-        dat->message = buffer;
-        push(dat, info->que);
 
     }
+    return arg;
+}
+
+/**
+ * @brief Listens to open port 
+ * 
+ * Listens to an open port and will print any
+ * input over this connection. It takes in a thread 
+ * struct as a void argument and uses this to find 
+ * the socket details.
+ * 
+ * @param arg Informaiton for the thread
+ * @return void* struct which was passed into the function
+ */
+void* readMessage(void* arg){
+    thread * info = (thread*) arg;
+    int valread;
+    char buffer[1024] = {0};
+    // Read from command line, create message 
+    // and pass it to message queue
+    do{
+        valread = read(info->socket, buffer, 1024);
+        char * exit = "exit\n";
+        printf("From Client: %s", buffer);
+        char data;
+        *(info->flag) = strcmp(buffer, exit);
+        if(!(*(info->flag))){
+            close(info->socket);
+            return arg;
+        }
+        recv(info->socket,&data,1, MSG_PEEK);
+        memset(buffer, 0, sizeof(buffer));
+    }while(1);
     return arg;
 }
